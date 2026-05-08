25ed1a6fd57448a2f1023b854e97a7dd96901cf4
firoz shaik <shaikfirozahmad99@gmail.com>
2026-05-08 05:51:15 +0530

    Set up dynamic port configuration for the server

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (_, res) => {
   res.send("Coolify works!");
 });
 
-app.listen(3000);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
